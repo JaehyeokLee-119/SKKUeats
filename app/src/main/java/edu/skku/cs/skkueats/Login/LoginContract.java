f9951c46f8d4666b4b13de693fa8a12f9ce674d4
cho-to <nicole2974@g.skku.edu>
2022-11-15 03:59:37 +0900

    로그인 페이지 model, view 구현

--- app/src/main/java/edu/skku/cs/skkueats/Login/LoginContract.java
+++ app/src/main/java/edu/skku/cs/skkueats/Login/LoginContract.java
@@ -3,8 +3,13 @@ package edu.skku.cs.skkueats.Login;
 public interface LoginContract {
     interface contactView {
         void initView();
+
+        void loginSuccess();
+
+        void loginFail();
     }
 
     interface contactModel {
+        void verifyId(String id, String password);
     }
 }
