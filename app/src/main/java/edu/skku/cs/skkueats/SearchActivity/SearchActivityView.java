26c79a7c449f8eb5e6b48e0b8b141042ce2d3efe
Lee Jae Hyeok <hjl88190705@gmail.com>
2022-11-14 15:19:05 +0900

    로그인, 검색 기능 실험환경 및 액티비티 구현중

--- app/src/main/java/edu/skku/cs/skkueats/SearchActivity/SearchActivityView.java
+++ app/src/main/java/edu/skku/cs/skkueats/SearchActivity/SearchActivityView.java
@@ -18,7 +18,7 @@ public class SearchActivityView extends AppCompatActivity implements SearchActiv
     private SearchActivityModel model;
 
     /*
-
+    검색 버튼을 누르면 버튼 아래 ListView에 검색결과를 search_result_item로 나열해서 보여줌
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +29,8 @@ public class SearchActivityView extends AppCompatActivity implements SearchActiv
         /*
         View 생성시:
             0. Activity에 있는 View를 초기화해줌
-
+            1. 버튼을 누르면 model이 검색어, 검색조건(가게이름, 메뉴이름)을 바탕으로 쿼리를 보내서 결과를 받아옴
+            2. 모델은 결과를 받으면 View에게 showRecommend로 결과를 표시하게 함
          */
 
         initView();
@@ -46,7 +47,7 @@ public class SearchActivityView extends AppCompatActivity implements SearchActiv
     @Override
     public void showRecommend(SearchResult searchResult) {
         /*
-        받은 정보를 바탕으로 ListView에 Review를 추가하여 화면에 표시한다
+        받은 SearchResult를 바탕으로 ListView에 Search Result를 추가하여 화면에 표시한다
          */
         searchResultArray.add(new SearchResult(searchResult));
 //        restaurantReviewAdapter = new MenuRecommendsAdapter(getApplicationContext(), recommendsArray);
