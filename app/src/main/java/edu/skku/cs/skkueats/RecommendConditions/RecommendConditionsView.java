b309dd384b07e1b4ec3d48399a52d9b32775f1f3
Lee Jae Hyeok <hjl88190705@gmail.com>
2022-11-15 15:23:46 +0900

    추천조건설정 화면 작업

--- app/src/main/java/edu/skku/cs/skkueats/RecommendConditions/RecommendConditionsView.java
+++ app/src/main/java/edu/skku/cs/skkueats/RecommendConditions/RecommendConditionsView.java
@@ -1,15 +1,23 @@
 package edu.skku.cs.skkueats.RecommendConditions;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 
+import edu.skku.cs.skkueats.MenuRecommendedList.MenuRecommendsView;
 import edu.skku.cs.skkueats.R;
+import edu.skku.cs.skkueats.RestaurantInfo.RestaurantInfoView;
 
 
 public class RecommendConditionsView extends AppCompatActivity implements RecommendConditionsContract.contactView {
@@ -24,8 +32,19 @@ public class RecommendConditionsView extends AppCompatActivity implements Recomm
     ConstraintLayout boxMinGrade;
     ConstraintLayout boxLocation;
 
+    RadioGroup bigCategoryCondition;
+    RadioGroup smallCategoryCondition;
+    ConstraintLayout priceCondition;
+    ConstraintLayout minGradeCondition;
+    RadioGroup locationCondition;
 
+    TextView textViewBigCategoryArrow;
+    TextView textViewSmallCategoryArrow;
+    TextView textViewPriceArrow;
+    TextView textViewMinGradeArrow;
+    TextView textViewLocationArrow;
 
+    private int visibilityLevel; // 화면에 표시되는 조건의 개수
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +65,51 @@ public class RecommendConditionsView extends AppCompatActivity implements Recomm
 
         initView();
 
-        //model = new RecommendConditionsModel(this, recommendQueryCondition);
+        boxSmallCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (visibilityLevel <= 1)
+                    visibilityLevel = 2;
+                showConditions(visibilityLevel);
+            }
+        });
+
+        boxPrice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (visibilityLevel <= 2)
+                    visibilityLevel = 3;
+                showConditions(visibilityLevel);
+            }
+        });
+
+        boxMinGrade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (visibilityLevel <= 3)
+                    visibilityLevel = 4;
+                showConditions(visibilityLevel);
+            }
+        });
+
+        boxLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (visibilityLevel <= 4)
+                    visibilityLevel = 5;
+                showConditions(visibilityLevel);
+            }
+        });
+
+        completeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MenuRecommendsView.class);
+                String[] conditions = new String[]{"대분류", "소분류", "5000", "3.5", "위치", "5"};
+                intent.putExtra("conditions", conditions);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -58,5 +121,98 @@ public class RecommendConditionsView extends AppCompatActivity implements Recomm
         boxPrice = findViewById(R.id.constraintLayoutPrice);
         boxMinGrade = findViewById(R.id.constraintLayoutMinGrade);
         boxLocation = findViewById(R.id.constraintLayoutLocation);
+
+        bigCategoryCondition = findViewById(R.id.BigCategoryCondition);
+        smallCategoryCondition = findViewById(R.id.SmallCategoryCondition);
+        priceCondition = findViewById(R.id.PriceCondition);
+        minGradeCondition = findViewById(R.id.MinGradeCondition);
+        locationCondition = findViewById(R.id.LocationCondition);
+
+        textViewBigCategoryArrow = findViewById(R.id.textViewBigCategoryArrow);
+        textViewSmallCategoryArrow = findViewById(R.id.textViewSmallCategoryArrow);
+        textViewPriceArrow = findViewById(R.id.textViewPriceArrow);
+        textViewMinGradeArrow = findViewById(R.id.textViewMinGradeArrow);
+        textViewLocationArrow = findViewById(R.id.textViewLocationArrow);
+
+        // 초기에는 대분류 빼곤 다 비활성화상태
+        smallCategoryCondition.setVisibility(View.GONE);
+        boxPrice.setVisibility(View.GONE);
+        priceCondition.setVisibility(View.GONE);
+        boxMinGrade.setVisibility(View.GONE);
+        minGradeCondition.setVisibility(View.GONE);
+        boxLocation.setVisibility(View.GONE);
+        locationCondition.setVisibility(View.GONE);
+
+        visibilityLevel = 1;
+        showConditions(visibilityLevel);
+    }
+
+    public void showConditions(int visibilityLevel) {
+        if (visibilityLevel >= 0) {
+            boxBigCategory.setVisibility(View.VISIBLE);
+            if (visibilityLevel == 0) {
+                textViewBigCategoryArrow.setTextColor(ContextCompat.getColor(getApplicationContext(),
+                        R.color.black));
+            } else {
+                textViewBigCategoryArrow.setTextColor(ContextCompat.getColor(getApplicationContext(),
+                        R.color.blackGrey));
+            }
+        }
+
+        if (visibilityLevel >= 1) {
+            boxBigCategory.setVisibility(View.VISIBLE);
+            bigCategoryCondition.setVisibility(View.VISIBLE);
+            boxSmallCategory.setVisibility(View.VISIBLE);
+            if (visibilityLevel == 1) {
+                textViewSmallCategoryArrow.setTextColor(ContextCompat.getColor(getApplicationContext(),
+                        R.color.black));
+            } else {
+                textViewSmallCategoryArrow.setTextColor(ContextCompat.getColor(getApplicationContext(),
+                        R.color.blackGrey));
+            }
+        }
+
+        if (visibilityLevel >= 2) {
+            smallCategoryCondition.setVisibility(View.VISIBLE);
+            boxPrice.setVisibility(View.VISIBLE);
+            if (visibilityLevel == 2) {
+                textViewPriceArrow.setTextColor(ContextCompat.getColor(getApplicationContext(),
+                        R.color.black));
+            } else {
+                textViewPriceArrow.setTextColor(ContextCompat.getColor(getApplicationContext(),
+                        R.color.blackGrey));
+            }
+        }
+
+        if (visibilityLevel >= 3) {
+            priceCondition.setVisibility(View.VISIBLE);
+            boxMinGrade.setVisibility(View.VISIBLE);
+            if (visibilityLevel == 3) {
+                textViewMinGradeArrow.setTextColor(ContextCompat.getColor(getApplicationContext(),
+                        R.color.black));
+            } else {
+                textViewMinGradeArrow.setTextColor(ContextCompat.getColor(getApplicationContext(),
+                        R.color.blackGrey));
+            }
+        }
+
+        if (visibilityLevel >= 4) {
+            minGradeCondition.setVisibility(View.VISIBLE);
+            boxLocation.setVisibility(View.VISIBLE);
+            if (visibilityLevel == 4) {
+                textViewLocationArrow.setTextColor(ContextCompat.getColor(getApplicationContext(),
+                        R.color.black));
+            } else {
+                textViewLocationArrow.setTextColor(ContextCompat.getColor(getApplicationContext(),
+                        R.color.blackGrey));
+            }
+        }
+
+        if (visibilityLevel >= 5) {
+            locationCondition.setVisibility(View.VISIBLE);
+            textViewLocationArrow.setTextColor(ContextCompat.getColor(getApplicationContext(),
+                    R.color.blackGrey));
+        }
+
     }
 }
